8dbd1d7a046d807730eb56c636fa876c4d5cfb45
Giorgio Armani <Giorgio Armani@DESKTOP-3ALTTPJ>
2023-04-12 11:41:23 +0200

    Add project files.

---
+++ ZamianaLiczebników/Program.cs
@@ -0,0 +1,31 @@
+﻿
+static string GłówneNaPorządkowe(int liczba)
+{
+    switch (liczba)
+    {
+        case 11:
+        case 12:
+        case 13:
+            return $"{liczba}th";
+        default:
+            int ostatniaCyfra = liczba % 10;
+
+            string przyrostek = ostatniaCyfra switch
+            {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+            };
+            return $"{liczba}{przyrostek}";
+    }
+}
+static void ZmieńGłówneNaPorządkowe()
+{
+    for (int liczba = 1; liczba <= 40; liczba++)
+    {
+        Console.WriteLine($"{GłówneNaPorządkowe(liczba)}");
+    }
+    Console.WriteLine();
+}
+ZmieńGłówneNaPorządkowe();
